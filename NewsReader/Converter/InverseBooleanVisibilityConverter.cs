35bd3ae9e89ce61f91207ae645ed629e9c2fd52b
DTeuchert <denny.teuchert@web.de>
2018-03-18 22:14:44 +0100

    Add IsEnabled property in rssLink model

---
+++ NewsReader/Converter/InverseBooleanVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NewsReader.Converter
+{
+    class InverseBooleanVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NewsReader/Model/RSSLink.cs
+++ NewsReader/Model/RSSLink.cs
@@ -30,5 +30,16 @@ namespace NewsReader.Model
                 OnPropertyChanged(nameof(Link));
             }
         }
+
+        private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged(nameof(IsEnabled));
+            }
+        }
     }
 }
