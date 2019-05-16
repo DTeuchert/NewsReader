2393b6552128da2399e1e562f3e577a822174d21
DTeuchert <denny.teuchert@web.de>
2019-05-16 08:13:30 +0200

    Refactor tab visibility binding

---
+++ NewsReader/ViewModel/CategoryVisibilityViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using NewsReader.Models;
+using NewsReader.Service;
+
+namespace NewsReader.ViewModel
+{
+    internal class CategoryVisibilityViewModel : BaseViewModel
+    {
+        private readonly Dictionary<RssCategory, bool> _visibleCategories;
+
+        public CategoryVisibilityViewModel()
+        {
+            _visibleCategories = ConfigurationService.VisibleCategories;
+        }
+
+        public bool General
+        {
+            get => _visibleCategories[RssCategory.General];
+            set
+            {
+                _visibleCategories[RssCategory.General] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(General));
+            }
+        }
+
+        public bool Sport
+        {
+            get => _visibleCategories[RssCategory.Sport];
+            set
+            {
+                _visibleCategories[RssCategory.Sport] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Sport));
+            }
+        }
+
+        public bool Technology
+        {
+            get => _visibleCategories[RssCategory.Technology];
+            set
+            {
+                _visibleCategories[RssCategory.Technology] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Technology));
+            }
+        }
+
+        public bool Health
+        {
+            get => _visibleCategories[RssCategory.Health];
+            set
+            {
+                _visibleCategories[RssCategory.Health] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Health));
+            }
+        }
+
+        public bool Economy
+        {
+            get => _visibleCategories[RssCategory.Economy];
+            set
+            {
+                _visibleCategories[RssCategory.Economy] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Economy));
+            }
+        }
+
+        public bool Career
+        {
+            get => _visibleCategories[RssCategory.Career];
+            set
+            {
+                _visibleCategories[RssCategory.Career] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Career));
+            }
+        }
+
+        public bool International
+        {
+            get => _visibleCategories[RssCategory.International];
+            set
+            {
+                _visibleCategories[RssCategory.International] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(International));
+            }
+        }
+
+        public bool Politics
+        {
+            get => _visibleCategories[RssCategory.Politics];
+            set
+            {
+                _visibleCategories[RssCategory.Politics] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Politics));
+            }
+        }
+
+        public bool Cultural
+        {
+            get => _visibleCategories[RssCategory.Cultural];
+            set
+            {
+                _visibleCategories[RssCategory.Cultural] = value;
+                OnChange_RSSCategory();
+                OnPropertyChanged(nameof(Cultural));
+            }
+        }
+
+        private void OnChange_RSSCategory()
+        {
+            ConfigurationService.VisibleCategories = _visibleCategories;
+        }
+    }
+}
--- NewsReader/ViewModel/MainWindowViewModel.cs
+++ NewsReader/ViewModel/MainWindowViewModel.cs
@@ -1,30 +1,31 @@
-﻿using NewsReader.Models;
-using NewsReader.Service;
-using NewsReader.Util;
-using NewsReader.View;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Windows;
 using System.Windows.Input;
 using System.Xml;
+using NewsReader.Models;
+using NewsReader.Service;
+using NewsReader.Util;
+using NewsReader.View;
 
 namespace NewsReader.ViewModel
 {
     class MainWindowViewModel : BaseViewModel
     {
-        private readonly List<string> _guidList = new List<string>(); 
+        private readonly List<string> _guidList = new List<string>();
 
         private bool _isConfigurationControlVisible;
-        public bool IsConfigurationControlVisible 
+        public bool IsConfigurationControlVisible
         {
             get => _isConfigurationControlVisible;
-            set {
+            set
+            {
                 _isConfigurationControlVisible = value;
                 OnPropertyChanged(nameof(IsConfigurationControlVisible));
             }
-        
+
         }
 
         private bool _isBookmarkControlVisible;
@@ -40,10 +41,11 @@ namespace NewsReader.ViewModel
         }
 
         private DateTimeOffset _lastUpdate = DateTimeOffset.Now;
-        public DateTimeOffset LastUpdate 
-        { 
+        public DateTimeOffset LastUpdate
+        {
             get => _lastUpdate;
-            set {
+            set
+            {
                 _lastUpdate = value;
                 OnPropertyChanged(nameof(LastUpdate));
             }
@@ -111,7 +113,7 @@ namespace NewsReader.ViewModel
 
         private ICommand _refreshCommand;
         public ICommand RefreshCommand => _refreshCommand ??
-            (_refreshCommand = new RelayCommand(() => UpdateFeedList()));
+            (_refreshCommand = new RelayCommand(UpdateFeedList));
 
         private ICommand _settingsCommand;
         public ICommand SettingsCommand => _settingsCommand ??
@@ -136,114 +138,11 @@ namespace NewsReader.ViewModel
                    }
                }));
 
-        private readonly Dictionary<RssCategory, bool> _visibleCategories = new Dictionary<RssCategory, bool>
-        {
-            { RssCategory.General, true },
-            { RssCategory.Sport, true },
-            { RssCategory.Technology, true },
-            { RssCategory.Health, true },
-            { RssCategory.Economy, true },
-            { RssCategory.Career, true },
-            { RssCategory.International, true },
-            { RssCategory.Politics, true },
-            { RssCategory.Cultural, true }
-        };
-                
-        public bool TabVisibility_General 
-        {
-            get => _visibleCategories[RssCategory.General];
-            set 
-            {
-                _visibleCategories[RssCategory.General] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_General));
-            } 
-        }   
-        public bool TabVisibility_Sport
-        {
-            get => _visibleCategories[RssCategory.Sport];
-            set
-            {
-                _visibleCategories[RssCategory.Sport] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Sport));
-            }
-        }
-        public bool TabVisibility_Technology
-        {
-            get => _visibleCategories[RssCategory.Technology];
-            set
-            {
-                _visibleCategories[RssCategory.Technology] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Technology));
-            }
-        }
-        public bool TabVisibility_Health
-        {
-            get => _visibleCategories[RssCategory.Health];
-            set
-            {
-                _visibleCategories[RssCategory.Health] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Health));
-            }
-        }
-        public bool TabVisibility_Economy
-        {
-            get => _visibleCategories[RssCategory.Economy];
-            set
-            {
-                _visibleCategories[RssCategory.Economy] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Economy));
-            }
-        }
-        public bool TabVisibility_Career
-        {
-            get => _visibleCategories[RssCategory.Career];
-            set
-            {
-                _visibleCategories[RssCategory.Career] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Career));
-            }
-        }
-        public bool TabVisibility_International
-        {
-            get => _visibleCategories[RssCategory.International];
-            set
-            {
-                _visibleCategories[RssCategory.International] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_International));
-            }
-        }
-        public bool TabVisibility_Politics
-        {
-            get => _visibleCategories[RssCategory.Politics];
-            set
-            {
-                _visibleCategories[RssCategory.Politics] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Politics));
-            }
-        }
-        public bool TabVisibility_Cultural
-        {
-            get => _visibleCategories[RssCategory.Cultural];
-            set
-            {
-                _visibleCategories[RssCategory.Cultural] = value;
-                OnChange_RSSCategory();
-                OnPropertyChanged(nameof(TabVisibility_Cultural));
-            }
-        }
-
+        public CategoryVisibilityViewModel CategoryVisibility { get; set; }
 
         private RssLink _sourceList_SelectedItem;
         public RssLink SourceList_SelectedItem
-        { 
+        {
             get => _sourceList_SelectedItem;
             set
             {
@@ -252,13 +151,14 @@ namespace NewsReader.ViewModel
             }
         }
 
-        public MainWindowViewModel(){
+        public MainWindowViewModel()
+        {
 
             FeedList = new RssFeedCollection();
             BookmarkList = new RssFeedCollection();
+            CategoryVisibility = new CategoryVisibilityViewModel();
 
             SourceList = ConfigurationService.Links;
-            _visibleCategories = ConfigurationService.VisibleCategories;
             UpdateFeedList();
         }
 
@@ -268,16 +168,12 @@ namespace NewsReader.ViewModel
             {
                 ConfigurationService.Links = SourceList;
                 UpdateFeedList();
-            }          
+            }
         }
-        private void OnChange_RSSCategory()
+
+        private void UpdateFeedList()
         {
-            ConfigurationService.VisibleCategories = _visibleCategories;
-        }
-        
-        private void UpdateFeedList() 
-        {
-            if(IsRefreshing) { return; }
+            if (IsRefreshing) { return; }
 
             IsRefreshing = true;
             foreach (var feed in SourceList)
@@ -292,7 +188,7 @@ namespace NewsReader.ViewModel
         private void UpdateFeed(RssLink rssLink)
         {
             if (FeedList == null) { return; }
-                    
+
             try
             {
                 using (var reader = XmlReader.Create(rssLink.Link))
@@ -319,31 +215,31 @@ namespace NewsReader.ViewModel
 
                         news.Category.Add(RssCategory.General);
 
-                        if (item.Categories.Any(x => 
-                            x.Name.Equals("Sport") || 
+                        if (item.Categories.Any(x =>
+                            x.Name.Equals("Sport") ||
                             x.Name.Equals("Fußball"))) news.Category.Add(RssCategory.Sport);
-                        if (item.Categories.Any(x => 
-                            x.Name.Equals("Technology") || 
+                        if (item.Categories.Any(x =>
+                            x.Name.Equals("Technology") ||
                             x.Name.Equals("Digital"))) news.Category.Add(RssCategory.Technology);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("Gesundheit"))) news.Category.Add(RssCategory.Health);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("Wirtschaft"))) news.Category.Add(RssCategory.Economy);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("Karriere"))) news.Category.Add(RssCategory.Career);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("International"))) news.Category.Add(RssCategory.International);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("Politik"))) news.Category.Add(RssCategory.Politics);
-                        if (item.Categories.Any(x => 
+                        if (item.Categories.Any(x =>
                             x.Name.Equals("Kultur"))) news.Category.Add(RssCategory.Cultural);
-   
+
                         FeedList.Add(news);
                         _guidList.Add(news.Guid);
                     }
                 }
             }
-            catch(Exception)
+            catch (Exception)
             {
                 rssLink.IsValid = false;
             }
